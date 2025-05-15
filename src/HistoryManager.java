74468a3c1e376da7ecb865cce1db17224e5be416
zh.zhandos <ZhZhumanov@1cb.kz>
2025-05-15 18:42:18 +0500

    Сделал историю задач неограниченной по размеру. Убрал повторные просмотры в истории.

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public interface HistoryManager {
     void add(Task task);
 
-    void remove(int id);
+//    void remove(int id);
 
     ArrayList<Task> getHistory();
 }
--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,32 +1,68 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
     ArrayList<Task> taskHistory;
+    ArrayList<Node> linkedHistory;
+    HashMap<Integer, Node> indexForLinkedHistory;
 
     public InMemoryHistoryManager() {
-        taskHistory = new ArrayList<>();
+        this.taskHistory = new ArrayList<>();
+        this.indexForLinkedHistory = new HashMap<>();
     }
 
     @Override
     public ArrayList<Task> getHistory() {
-        return this.taskHistory;
+        return this.getTasks();
     }
 
     @Override
     public void add(Task task) {
-        if (this.taskHistory.size() == 10) {
-            this.taskHistory.removeFirst();
+        if (this.indexForLinkedHistory.containsKey(task.getId())) {
+            this.removeNode(this.indexForLinkedHistory.get(task.getId()));
         }
-        this.taskHistory.add(task);
+        this.linkLast(task);
     }
 
-    @Override
-    public void remove(int id) {
-        for (Task task : taskHistory) {
-            if (task.getId() == id) {
-                this.taskHistory.remove(task);
-                break;
-            }
+//    @Override
+//    public void remove(int id) {
+//        for (Task task : taskHistory) {
+//            if (task.getId() == id) {
+//                this.taskHistory.remove(task);
+//                break;
+//            }
+//        }
+//    }
+
+    void linkLast(Task task) {
+        if (this.linkedHistory.isEmpty()) {
+            Node newNode = new Node(null, task, null);
+            this.linkedHistory.add(newNode);
+            this.indexForLinkedHistory.put(task.getId(), newNode);
         }
+        else {
+            Node prevNode = this.linkedHistory.getLast();
+            Node newNode = new Node(prevNode, task, null);
+            this.linkedHistory.add(newNode);
+            prevNode.setNextNode(newNode);
+            this.indexForLinkedHistory.put(task.getId(), newNode);
+        }
+    }
+
+    ArrayList<Task> getTasks() {
+        ArrayList<Task> result = new ArrayList<>();
+        for (Node node : this.linkedHistory) {
+            result.add(node.task);
+        }
+        return result;
+    }
+
+    void removeNode(Node node) {
+        int taskIdToRemove = node.getTask().getId();
+        Node prevNode = node.getPrevNode();
+        Node nextNode = node.getNextNode();
+        prevNode.setNextNode(nextNode);
+        nextNode.setPrevNode(prevNode);
+        this.indexForLinkedHistory.remove(taskIdToRemove);
     }
 }
